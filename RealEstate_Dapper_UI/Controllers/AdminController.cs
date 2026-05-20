888a3bddb9e53ab7ceed6b4f715de7e5871d09e9
POOL <astonov77@gmail.com>
2026-05-20 15:12:52 +0300

    feat: implement admin layout components and category management structure

---
+++ RealEstate_Dapper_UI/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: AdminController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ RealEstate_Dapper_UI/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: CategoryController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
