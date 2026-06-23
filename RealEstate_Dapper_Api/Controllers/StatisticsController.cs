362c947b44fbebb8d1085ebf85095fe15ebf8fb0
POOL <astonov77@gmail.com>
2026-06-23 10:08:26 +0300

    feat: implement statistics repository and controller for real-time data tracking

---
+++ RealEstate_Dapper_Api/Controllers/StatisticsController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.StatisticsRepository;
+
+namespace RealEstate_Dapper_Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StatisticsController : ControllerBase
+{
+    private readonly IStatisticsRepository _statisticsRepository;
+
+    public StatisticsController(IStatisticsRepository statisticsRepository)
+    {
+        _statisticsRepository = statisticsRepository;
+    }
+
+    [HttpGet("ActiveCategoryCount")]
+    public IActionResult ActiveCategoryCount()
+    {
+        return Ok(_statisticsRepository.ActiveCategoryCount());
+    }
+
+    [HttpGet("ActiveEmployeeCount")]
+    public IActionResult ActiveEmployeeCount()
+    {
+        return Ok(_statisticsRepository.ActiveEmployeeCount());
+    }
+
+}
---
+++ RealEstate_Dapper_Api/Repositories/StatisticsRepository/IStatisticsRepository.cs
@@ -0,0 +1,19 @@
+namespace RealEstate_Dapper_Api.Repositories.StatisticsRepository;
+
+public interface IStatisticsRepository
+{
+    int CategoryCount();
+    int ActiveCategoryCount();
+    int PassiveCategoryCount();
+    int ProductCount();
+    int ApartmentCount();
+    string EmployeeNameByMaxProductCount();
+    decimal AverageProductBySale();
+    string CityNameByMaxProductCount();
+    int DifferentCityCount();
+    decimal LastProductPrice();
+    string NewestBuildingYear();
+    string OldestBuildingYear();
+    int AverageRoomCount();
+    int ActiveEmployeeCount();
+}
